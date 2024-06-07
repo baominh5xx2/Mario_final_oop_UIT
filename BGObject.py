37c886087c0246f629ea3da7c8b0a1d951cc08f6
MINHBAO <151294761+baominh5xx2@users.noreply.github.com>
2024-06-08 02:12:24 +0700

    BGObject.py

--- BGObject.py
+++ BGObject.py
@@ -1,5 +1,21 @@
 import pygame as pg
+"""
+    Một lớp đại diện cho các đối tượng nền trong trò chơi.
 
+    Thuộc tính
+    ----------
+    rect : pygame.Rect
+        Hình chữ nhật đại diện cho vị trí và kích thước của đối tượng nền.
+    image : pygame.Surface
+        Hình ảnh sẽ được hiển thị cho đối tượng nền.
+    type : str
+        Loại của đối tượng, mặc định là 'BGObject'.
+
+    Phương thức
+    ----------
+    render(core):
+        Hiển thị đối tượng nền lên màn hình.
+    """
 
 class BGObject(object):
     def __init__(self, x, y, image):
