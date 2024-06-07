4a0dc60350bb3a1e9b43afb27b37496357b824b7
MINHBAO <151294761+baominh5xx2@users.noreply.github.com>
2024-06-08 02:19:56 +0700

    Const.py

--- Const.py
+++ Const.py
@@ -1,8 +1,34 @@
 WINDOW_W = 1000
 WINDOW_H = 448
 FPS = 100
+"""
+Tệp chứa các hằng số được sử dụng trong trò chơi.
 
-# Player physics
+Hằng số
+-------
+WINDOW_W : int
+    Chiều rộng của cửa sổ trò chơi.
+WINDOW_H : int
+    Chiều cao của cửa sổ trò chơi.
+FPS : int
+    Số khung hình trên giây.
+SPEED_INCREASE_RATE : float
+    Tốc độ tăng vận tốc.
+SPEED_DECREASE_RATE : float
+    Tốc độ giảm vận tốc.
+MAX_MOVE_SPEED : float
+    Vận tốc di chuyển tối đa.
+MAX_FASTMOVE_SPEED : float
+    Vận tốc di chuyển nhanh tối đa.
+GRAVITY : float
+    Gia tốc trọng trường.
+LOW_JUMP_MULTIPLIER : float
+    Hệ số giảm nhảy thấp.
+FALL_MULTIPLIER : float
+    Hệ số giảm khi rơi.
+JUMP_POWER : float
+    Sức mạnh nhảy.
+"""
 GRAVITY = 0.08
 SPEED_INCREASE_RATE = 0.038
 SPEED_DECREASE_RATE = 0.038
