660d7e38feabf11f2afe425e80a6a7c3876cd890
MINHBAO <151294761+baominh5xx2@users.noreply.github.com>
2024-06-08 02:26:07 +0700

    Text.py

--- Text.py
+++ Text.py
@@ -1,5 +1,33 @@
 import pygame as pg
+"""
+    Lớp để quản lý văn bản trong trò chơi.
 
+    Thuộc tính
+    ----------
+    text : str
+        Văn bản cần hiển thị.
+    size : int
+        Kích thước của font chữ.
+    position : tuple
+        Vị trí của văn bản.
+    font : str
+        Font chữ của văn bản.
+    textcolor : tuple
+        Màu sắc của văn bản.
+    font : pygame.font.Font
+        Đối tượng font chữ của pygame.
+    surface : pygame.Surface
+        Bề mặt hiển thị của văn bản.
+    rect : pygame.Rect
+        Hình chữ nhật đại diện cho vị trí và kích thước của văn bản.
+
+    Phương thức
+    ----------
+    render(core):
+        Hiển thị văn bản lên màn hình.
+    render_in_game(core):
+        Hiển thị văn bản lên màn hình trong trò chơi.
+    """
 class Text(object):
     def __init__(self, text, fontsize, rectcenter, font='Emulogic', textcolor=(255, 255, 255)):
         self.font = pg.font.Font('fonts/emulogic.ttf', fontsize)
