1383bb6480319cc49e9464564071a822d236784a
MINHBAO <151294761+baominh5xx2@users.noreply.github.com>
2024-06-08 02:18:13 +0700

    GameOverMenu.py

--- GameOverMenu.py
+++ GameOverMenu.py
@@ -1,7 +1,25 @@
 import pygame as pg
 from Text import Text
 from Const import WINDOW_W, WINDOW_H
+"""
+    Lớp để quản lý menu kết thúc trò chơi.
 
+    Thuộc tính
+    ----------
+    screen : pygame.Surface
+        Bề mặt hiển thị của menu kết thúc trò chơi.
+    font : pygame.font.Font
+        Font chữ của menu kết thúc trò chơi.
+    text : pygame.Surface
+        Văn bản hiển thị trên menu kết thúc trò chơi.
+    text_rect : pygame.Rect
+        Hình chữ nhật đại diện cho vị trí và kích thước của văn bản.
+
+    Phương thức
+    ----------
+    render():
+        Hiển thị menu kết thúc trò chơi lên màn hình.
+    """
 class GameOverMenu:
     def __init__(self, core):
         self.core = core
